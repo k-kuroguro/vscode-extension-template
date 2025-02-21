ecb46c9d8e163c89ab9161566313b4682f7f328b
k-kuroguro <lingjiujinshan@gmail.com>
2025-02-21 22:54:09 +0900

    refactor: Change logger args to readonly for immutability

--- src/logger.ts
+++ src/logger.ts
@@ -2,11 +2,13 @@ import * as util from 'node:util';
 import { type LogOutputChannel, window } from 'vscode';
 import { extensionDisplayName } from './constants';
 
+type Arguments = readonly unknown[];
+
 export interface Logger {
-   debug(...data: unknown[]): void;
-   info(...data: unknown[]): void;
-   warn(...data: unknown[]): void;
-   error(...data: unknown[]): void;
+   debug(...data: Arguments): void;
+   info(...data: Arguments): void;
+   warn(...data: Arguments): void;
+   error(...data: Arguments): void;
 }
 
 export class OutputChannelLogger implements Logger {
@@ -16,19 +18,19 @@ export class OutputChannelLogger implements Logger {
       this.channel = window.createOutputChannel(extensionDisplayName, { log: true });
    }
 
-   debug(...data: unknown[]): void {
+   debug(...data: Arguments): void {
       this.channel.debug(util.format(...data));
    }
 
-   info(...data: unknown[]): void {
+   info(...data: Arguments): void {
       this.channel.info(util.format(...data));
    }
 
-   warn(...data: unknown[]): void {
+   warn(...data: Arguments): void {
       this.channel.warn(util.format(...data));
    }
 
-   error(...data: unknown[]): void {
+   error(...data: Arguments): void {
       this.channel.error(util.format(...data));
    }
 
