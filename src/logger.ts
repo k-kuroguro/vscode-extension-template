da52767ad7fbe4f599ed22114742a582b969b05d
k-kuroguro <lingjiujinshan@gmail.com>
2025-03-03 20:44:22 +0900

    feat: Add log method to logger

--- src/logger.ts
+++ src/logger.ts
@@ -4,7 +4,16 @@ import { extensionDisplayName } from './constants';
 
 type Arguments = readonly unknown[];
 
+export const LogLevel = {
+   Debug: 'debug',
+   Info: 'info',
+   Warn: 'warn',
+   Error: 'error',
+} as const;
+export type LogLevel = (typeof LogLevel)[keyof typeof LogLevel];
+
 export interface Logger {
+   log: (level: LogLevel, ...data: Arguments) => void;
    debug(...data: Arguments): void;
    info(...data: Arguments): void;
    warn(...data: Arguments): void;
@@ -16,6 +25,23 @@ export class OutputChannelLogger implements Logger {
 
    constructor() {
       this.channel = window.createOutputChannel(extensionDisplayName, { log: true });
+   }
+
+   log(level: LogLevel, ...data: Arguments): void {
+      switch (level) {
+         case LogLevel.Debug:
+            this.debug(...data);
+            break;
+         case LogLevel.Info:
+            this.info(...data);
+            break;
+         case LogLevel.Warn:
+            this.warn(...data);
+            break;
+         case LogLevel.Error:
+            this.error(...data);
+            break;
+      }
    }
 
    debug(...data: Arguments): void {
