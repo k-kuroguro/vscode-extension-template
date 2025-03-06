ca00c50e15c5abc2a79b667d031fa56989958814
k-kuroguro <lingjiujinshan@gmail.com>
2025-03-06 22:01:23 +0900

    refactor: Improve event naming

--- src/config.ts
+++ src/config.ts
@@ -2,19 +2,19 @@ import * as vscode from 'vscode';
 import { extensionName } from './constants';
 
 export class Config {
-   private _onDidChangeConfig: vscode.EventEmitter<Config.ConfigItems> = new vscode.EventEmitter<Config.ConfigItems>();
-   readonly onDidChangeConfig: vscode.Event<Config.ConfigItems> = this._onDidChangeConfig.event;
+   private _onDidChange: vscode.EventEmitter<Config.ConfigItems> = new vscode.EventEmitter<Config.ConfigItems>();
+   readonly onDidChange: vscode.Event<Config.ConfigItems> = this._onDidChange.event;
 
    private static instance: Config = new Config();
    private workspaceConfig: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(extensionName);
-   private disposables: vscode.Disposable[] = [this._onDidChangeConfig];
+   private disposables: vscode.Disposable[] = [this._onDidChange];
 
    private constructor() {
       this.disposables.push(
          vscode.workspace.onDidChangeConfiguration((e) => {
             this.loadWorkspaceConfig();
             const changedItems = Object.values(Config.ConfigItem).filter((item) => e.affectsConfiguration(`${extensionName}.${item}`));
-            if (changedItems.length) this._onDidChangeConfig.fire(changedItems);
+            if (changedItems.length) this._onDidChange.fire(changedItems);
          }),
       );
    }
@@ -39,7 +39,7 @@ export class Config {
 
    set useWarning(useWarning: boolean) {
       this.workspaceConfig.update(Config.ConfigItem.UseWarning, useWarning, vscode.ConfigurationTarget.Global);
-      this._onDidChangeConfig.fire([Config.ConfigItem.UseWarning]);
+      this._onDidChange.fire([Config.ConfigItem.UseWarning]);
    }
 }
 
